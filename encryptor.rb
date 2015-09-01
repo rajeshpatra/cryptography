192d08eac8ad2dbd03cfa4bbdda4e64824242540
Rajesh Patra <p7.rajesh@gmail.com>
2015-09-01 19:54:06 +0530

    added encrypt for one letter

---
+++ encryptor.rb
@@ -0,0 +1,15 @@
+class Cryptography
+	def cipher
+		{'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm'}
+	end
+
+	def encrypt(letter)
+		cipher[letter]
+	end
+end
